f55a812ba55a5a4be1d4a3fb0ab2fbb99b1337b8
István Miklós Antal <istvan.m.antal@gmail.com>
2011-06-23 10:17:54 +0200

    Deb build file fixed.

--- build/wt/build.sh
+++ build/wt/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -rf debian/usr/share/jsop/*
-cp -r ../../* debian/usr/share/jsop
-rm -rf debian/usr/share/jsop/nbproject
-rm -rf debian/usr/share/jsop/pkg
+rm -rf debian/usr/share/wt/*
+cp -r ../../* debian/usr/share/wt
+rm -rf debian/usr/share/wt/nbproject
+rm -rf debian/usr/share/wt/pkg
 dpkg-deb --build debian
 mv debian.deb wt-$1-all.deb
-rm -rf debian/usr/share/jsop/*
+rm -rf debian/usr/share/wt/*
