822551908c97fcfe82c389a265b4e862592f601f
Antal István Miklós <istvan.m.antal@gmail.com>
2012-08-22 17:20:09 +0200

    PHP linting and pre-commit support for git

---
+++ tests/bad/Foo.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Foo
+ *
+ * @author void
+ */
+class Foo {
+    //put your code here
+    public static function foo_bar($bar,$foo) {
+        
+    }
+}
--- usr/Commands.php
+++ usr/Commands.php
@@ -1,5 +1,6 @@
 <?php
-/*!
+
+/* !
  * 
  * WebTool
  * http://www.istvan-antal.ro/wt.html
@@ -9,6 +10,7 @@
  * http://www.istvan-antal.ro/open-source.html
  * 
  */
+
 class Commands {
 
     public static function help() {
@@ -27,14 +29,14 @@ class Commands {
         echo WT::colorize('<modulename>', 'red');
         echo "`";
         echo WT::colorize(" - Creates a client side JS module object scaffold.\n", 'white');
-        
+
         // Module Create Function
         echo "`";
         echo WT::colorize('wt module create function ', 'light_blue');
         echo WT::colorize('<modulename>', 'red');
         echo "`";
         echo WT::colorize(" - Creates a client side JS module function scaffold.\n", 'white');
-        
+
         // Module Create Widget
         echo "`";
         echo WT::colorize('wt module create widget ', 'light_blue');
@@ -48,7 +50,7 @@ class Commands {
         echo WT::colorize('<file1.js file2.js ...>', 'red');
         echo "`";
         echo WT::colorize(" - Checks the files for bad coding habbits and errors.\n", 'white');
-        
+
         // Lint JS FilesWT::colorize(
         echo "`";
         echo WT::colorize('wt hint ', 'light_blue');
@@ -71,7 +73,7 @@ class Commands {
         echo WT::colorize('<output file>', 'light_red');
         echo "`";
         echo WT::colorize(" - Compiles the input files into an output file.\n", 'white');
-        
+
         // Build JS files
         echo "`";
         echo WT::colorize('wt build ', 'light_blue');
@@ -82,7 +84,7 @@ class Commands {
 
         echo "\n";
         echo WT::colorize("Local configuration:\n", 'white');
-        
+
         // Local Config set
         echo "`";
         echo WT::colorize('wt config set ', 'light_blue');
@@ -90,7 +92,7 @@ class Commands {
         echo WT::colorize('<value>', 'light_red');
         echo "`";
         echo WT::colorize(" - Sets a local value for a specific key.\n", 'white');
-        
+
         // Local Config unset
         echo "`";
         echo WT::colorize('wt config unset ', 'light_blue');
@@ -98,19 +100,19 @@ class Commands {
         echo WT::colorize('<value>', 'light_red');
         echo "`";
         echo WT::colorize(" - Unsets the local value for a specific key.\n", 'white');
-        
+
         // Local Config purge
         echo "`";
         echo WT::colorize('wt config purge', 'light_blue');
         echo "`";
         echo WT::colorize(" - Removes the local config file.\n", 'white');
-        
+
         // Local Config show
         echo "`";
         echo WT::colorize('wt config show', 'light_blue');
         echo "`";
         echo WT::colorize(" - Lists the current config keys.\n", 'white');
-        
+
         echo "\n";
         echo WT::colorize("Experimental:\n", 'white');
         // Complile JS files
@@ -120,12 +122,52 @@ class Commands {
         echo WT::colorize('<output file>', 'light_red');
         echo "`";
         echo WT::colorize(" - Compiles the input files into an output file using the advanced settings for cc.\n", 'white');
-        
+
         echo "\n";
         echo WT::colorize("References:\n", 'white');
         echo WT::colorize('JSDOC - ', 'light_gray') . WT::colorize("http://code.google.com/p/jsdoc-toolkit/", 'blue') . "\n";
         echo WT::colorize('Google Closure Compiler - ', 'light_gray') . WT::colorize("http://code.google.com/closure/compiler/", 'blue') . "\n";
         echo WT::colorize('JSLint - ', 'light_gray') . WT::colorize("http://www.jslint.com/", 'blue') . "\n";
+    }
+
+    public static function check($params) {
+        $cfile = '.wt.json';
+
+        if (count($params)) {
+            $cfile = $params[0];
+        }
+
+        $config = json_decode(file_get_contents($cfile), true);
+
+        exec('git diff --cached --name-only', $files);
+
+        foreach ($config['pre-commit'] as $group) {
+            foreach ($files as $file) {
+                if (fnmatch($group['files'], $file)) {
+                    switch ($group['job']) {
+                        case 'lintJS':
+                            system("wt lint " . ($group['node'] ? "node=true sloppy=true " : 'browser=true ') . "$file", $status);
+                            if ($status) {
+                                exit($status);
+                            }
+                            break;
+                        case 'lintPHP':
+                            system("wt lintPHP $file", $status);
+                            if ($status) {
+                                exit($status);
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    public static function setupGit() {
+        $sdir = WT::getScriptDir();
+        copy("$sdir/templates/pre-commit", '.git/hooks/pre-commit');
+        copy("$sdir/templates/wt.json", '.wt.json');
+        chmod('.git/hooks/pre-commit', 0755);
     }
 
     public static function module($params) {
@@ -174,7 +216,7 @@ class Commands {
                     }
                 }
 
-                system("java -jar $sdir/tools/jsdoc/jsrun.jar $sdir/tools/jsdoc/app/run.js $files -t=$sdir/tools/jsdoc/templates/".WT::getConf('jsdoc.theme')." -d=$docdir");
+                system("java -jar $sdir/tools/jsdoc/jsrun.jar $sdir/tools/jsdoc/app/run.js $files -t=$sdir/tools/jsdoc/templates/" . WT::getConf('jsdoc.theme') . " -d=$docdir");
 
                 WT::success("API documentation created.");
                 exit(0);
@@ -182,6 +224,40 @@ class Commands {
             default :
                 WT::error("Invalid documentation command.");
         }
+    }
+
+    public static function lintPHP($params, $terminate = true) {
+        $sdir = WT::getScriptDir();
+
+        $files = $params;
+
+        $cwd = getcwd();
+
+        foreach ($files as &$file) {
+            $file = "$cwd/$file";
+        }
+
+        $files = implode(' ', $files);
+
+        @exec("php $sdir/tools/phpcs/scripts/phpcs --standard=WT $files", $output, $status);
+
+        if ($status) {
+            foreach ($output as $line) {
+                if (!strlen($line)) {
+                    echo "\n";
+                } else {
+                    echo trim($line) . "\n";
+                }
+            }
+            echo "\n";
+            WT::error('Check failed');
+        } else {
+            WT::success("Check passed.", $terminate);
+        }
+    }
+
+    public static function lintJS($params, $terminate = true) {
+        self::lint($params, $terminate);
     }
 
     public static function lint($params, $terminate = true) {
@@ -202,7 +278,7 @@ class Commands {
             WT::success("Check passed.", $terminate);
         }
     }
-    
+
     public static function hint($params, $terminate = true) {
         $sdir = WT::getScriptDir();
         $options = array(
@@ -215,8 +291,8 @@ class Commands {
             'eqeqeq' => 'true',
             'curly' => 'true',
             'bitwise' => 'true'
-            );
-        
+        );
+
         foreach ($params as $k => $v) {
             if (strpos($v, '=')) {
                 unset($params[$k]);
@@ -227,21 +303,21 @@ class Commands {
                 }
             }
         }
-        
-        
+
+
         $optstr = '';
-        
+
         $t = array();
         foreach ($options as $k => $v) {
-            $t []= "$k=$v";
+            $t [] = "$k=$v";
         }
-        
+
         $optstr = implode(',', $t);
-        
+
         $files = implode(' ', $params);
         //echo "java -jar $sdir/tools/rhino/js.jar $sdir/tools/jshint/rhino.js $sdir/tools/jshint/jshint.js $optstr $files\n";
         @exec("java -jar $sdir/tools/rhino/js.jar $sdir/tools/jshint/rhino.js $sdir/tools/jshint/jshint.js $optstr $files", $output, $status);
-        
+
         if ($status) {
             foreach ($output as $line) {
                 if (!strlen($line)) {
@@ -277,7 +353,7 @@ class Commands {
             }
         }
 
-        system("java -jar $sdir/tools/closure/compiler.jar --compilation_level ".(($advanced)?'ADVANCED':'SIMPLE')."_OPTIMIZATIONS $files --js_output_file=$output");
+        system("java -jar $sdir/tools/closure/compiler.jar --compilation_level " . (($advanced) ? 'ADVANCED' : 'SIMPLE') . "_OPTIMIZATIONS $files --js_output_file=$output");
         WT::success("Build successful.");
     }
 
@@ -290,10 +366,10 @@ class Commands {
         Commands::lint($lfiles, false);
         Commands::compile($bfiles);
     }
-    
+
     public static function config($params) {
         $operation = array_shift($params);
-        
+
         switch ($operation) {
             case 'set':
                 $key = array_shift($params);
