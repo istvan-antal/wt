1d65f4225280cc0bb397b571878df84adb8f9a02
Antal István Miklós <istvan.m.antal@gmail.com>
2012-08-26 14:30:04 +0200

    Removed deb build script.

--- build/wt/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf debian/usr/share/wt/*
-cp -r ../../* debian/usr/share/wt
-rm -rf debian/usr/share/wt/nbproject
-rm -rf debian/usr/share/wt/build
-dpkg-deb --build debian
-mv debian.deb wt-$1-all.deb
-rm -rf debian/usr/share/wt/*
